00e889ad41070ba3df822d6292dbfe9c813e6b66
davitru60 <davitru60@gmail.com>
2023-02-16 11:49:44 +0100

    11:49

--- code/reusable-header.js
+++ code/reusable-header.js
@@ -6,10 +6,21 @@ function createHeader() {
         <a href="./main.html"> HOME</a>
         <a href="./market.html"> TIENDA</a>
         <a href="./blog.html"> BLOG</a>
+        <a href="./log-in.html"> 
+            <img src="/Imagenes/log-in-button.png" id="log-in-btn">
+        </a>
+
+        <a href="./register.html"> 
+            <img src="/Imagenes/register-button.png" id="register-button">
+        </a>
     </nav>
+    
     `;
+
+    
     return header;
 }
 
 document.body.appendChild(createHeader());
+
 
