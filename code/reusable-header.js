cf03f71f1696ac759972bed62566feaeeb6aca83
davitru60 <davitru60@gmail.com>
2023-02-20 16:52:29 +0100

    16:52

--- code/reusable-header.js
+++ code/reusable-header.js
@@ -1,17 +1,17 @@
 function createHeader() {
     let header = document.createElement("header");
     header.innerHTML = `
-    <img src="/Imagenes/NELogo.png">
+    <img src="/images/NELogo.png">
     <nav>
         <a href="./main.html"> HOME</a>
         <a href="./market.html"> TIENDA</a>
         <a href="./blog.html"> BLOG</a>
         <a href="./log-in.html"> 
-            <img src="/Imagenes/log-in-button.png" id="log-in-btn">
+            <img src="/images/log-in-button.png" id="log-in-btn">
         </a>
 
         <a href="./register.html"> 
-            <img src="/Imagenes/register-button.png" id="register-button">
+            <img src="/images/register-button.png" id="register-button">
         </a>
     </nav>
     
