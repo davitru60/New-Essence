b9cdfb3503ff3a726aad20b81bcc21af5d632cec
davitru60 <davitru60@gmail.com>
2023-02-22 09:29:53 +0100

    9:29

--- code/reusable-footer.js
+++ code/reusable-footer.js
@@ -1,7 +1,29 @@
 function createFooter(){
     let footer = document.createElement("footer");
     footer.innerHTML=`
-    <h1> FOOTER</h1>
+        <div id="creator-information">
+            <span> Cliente: Elena Rodriguez Calderón</span>
+            <span> Desarrollador: David Trujillo Carrero</span>
+        </div>
+
+        <div id="social-media">
+            <a href="https://www.instagram.com/" class="foot-tooltip instagram"> 
+                <img src="/images/social-media/Instagram_icon.png.webp" id="social-media-icon">
+            </a>
+            <a href="https://es-la.facebook.com/" class="foot-tooltip facebook"> 
+                <img src="/images/social-media/facebook-logo.png" id="social-media-icon">
+            </a>
+
+            <a href="https://twitter.com/?lang=es" class="foot-tooltip twitter"> 
+                <img src="/images/social-media/twitter-icom.webp" id="social-media-icon">
+            </a>
+
+            <a href="mailto:newessence@example.com" class="foot-tooltip outlook"> 
+                <img src="/images/social-media/outlook-icon.png" class="social-media-icon outlook-icon">
+            </a>
+        </div>
+
+        <img id="logo-2" src="/images/NELogo.png">
     `;
 
     return footer;
--- code/reusable-header.js
+++ code/reusable-header.js
@@ -1,16 +1,20 @@
 function createHeader() {
     let header = document.createElement("header");
     header.innerHTML = `
-    <img src="/images/NELogo.png">
+
+
+    <a href="./main.html"> 
+        <img src="/images/Logo_2.png" id="logo-1">
+    </a>
     <nav>
-        <a href="./main.html"> HOME</a>
-        <a href="./market.html"> TIENDA</a>
-        <a href="./blog.html"> BLOG</a>
-        <a href="./log-in.html"> 
+        <a href="./main.html" class="menu-option op1"> HOME</a>
+        <a href="./market.html" class="menu-option op2"> TIENDA</a>
+        <a href="./blog.html" class="menu-option op3"> BLOG</a>
+        <a href="./log-in.html" class="menu-option op4"> 
             <img src="/images/log-in-button.png" id="log-in-btn">
         </a>
 
-        <a href="./register.html"> 
+        <a href="./register.html"  class="menu-option op5"> 
             <img src="/images/register-button.png" id="register-button">
         </a>
     </nav>
