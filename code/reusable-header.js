08f2d23a11bf96b9510c78c133727cda3713d3f3
davitru60 <davitru60@gmail.com>
2023-03-07 00:10:04 +0100

    summary v2

--- code/reusable-header.js
+++ code/reusable-header.js
@@ -3,19 +3,19 @@ function createHeader() {
     header.innerHTML = `
 
 
-    <a href="index.html"> 
-        <img src="./images/Logo_2.png" id="logo-1">
+    <a href="/index.html"> 
+        <img src="/images/Logo_2.png" id="logo-1">
     </a>
     <nav>
-        <a href="index.html" class="menu-option op1"> HOME</a>
-        <a href="./../html-files/market.html" class="menu-option op2"> TIENDA</a>
-        <a href="./html-files/blog.html" class="menu-option op3"> BLOG</a>
-        <a href="./html-files/log-in.html" class="menu-option op4"> 
-            <img src="./images/log-in-button.png" id="log-in-btn">
+        <a href="/index.html" class="menu-option op1"> HOME</a>
+        <a href="/html-files/market.html" class="menu-option op2"> TIENDA</a>
+        <a href="/html-files/blog.html" class="menu-option op3"> BLOG</a>
+        <a href="/html-files/log-in.html" class="menu-option op4"> 
+            <img src="/images/log-in-button.png" id="log-in-btn">
         </a>
 
-        <a href="./html-files/register.html"  class="menu-option op5"> 
-            <img src="./images/register-button.png" id="register-button">
+        <a href="/html-files/register.html"  class="menu-option op5"> 
+            <img src="/images/register-button.png" id="register-button">
         </a>
     </nav>
     
