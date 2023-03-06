ba0fe5729f36dbcd4e717c8a48bfafcda6d1aac0
davitru60 <davitru60@gmail.com>
2023-03-06 23:58:12 +0100

    Update reusable-header.js

--- code/reusable-header.js
+++ code/reusable-header.js
@@ -8,14 +8,14 @@ function createHeader() {
     </a>
     <nav>
         <a href="index.html" class="menu-option op1"> HOME</a>
-        <a href="../html-files/market.html" class="menu-option op2"> TIENDA</a>
-        <a href="../html-files/blog.html" class="menu-option op3"> BLOG</a>
-        <a href="../html-files/log-in.html" class="menu-option op4"> 
-            <img src="../images/log-in-button.png" id="log-in-btn">
+        <a href="./html-files/market.html" class="menu-option op2"> TIENDA</a>
+        <a href="./html-files/blog.html" class="menu-option op3"> BLOG</a>
+        <a href="./html-files/log-in.html" class="menu-option op4"> 
+            <img src="./images/log-in-button.png" id="log-in-btn">
         </a>
 
-        <a href="../html-files/register.html"  class="menu-option op5"> 
-            <img src="../images/register-button.png" id="register-button">
+        <a href="./html-files/register.html"  class="menu-option op5"> 
+            <img src="./images/register-button.png" id="register-button">
         </a>
     </nav>
     
