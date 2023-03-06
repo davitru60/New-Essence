9a6e09fa294361fab82c4f56504d1c590973979a
davitru60 <davitru60@gmail.com>
2023-03-06 23:24:55 +0100

    Update reusable-footer.js

--- code/reusable-footer.js
+++ code/reusable-footer.js
@@ -11,19 +11,19 @@ function createFooter(){
                 <img src="../images/social-media/Instagram_icon.png.webp" id="social-media-icon">
             </a>
             <a href="https://es-la.facebook.com/" class="foot-tooltip facebook"> 
-                <img src="/images/social-media/facebook-logo.png" id="social-media-icon">
+                <img src="../images/social-media/facebook-logo.png" id="social-media-icon">
             </a>
 
             <a href="https://twitter.com/?lang=es" class="foot-tooltip twitter"> 
-                <img src="/images/social-media/twitter-icom.webp" id="social-media-icon">
+                <img src="../images/social-media/twitter-icom.webp" id="social-media-icon">
             </a>
 
             <a href="mailto:newessence@example.com" class="foot-tooltip outlook"> 
-                <img src="/images/social-media/outlook-icon.png" class="outlook-icon">
+                <img src="../images/social-media/outlook-icon.png" class="outlook-icon">
             </a>
         </div>
 
-        <img id="logo-2" src="/images/NELogo.png">
+        <img id="logo-2" src="../images/NELogo.png">
     `;
 
     return footer;
